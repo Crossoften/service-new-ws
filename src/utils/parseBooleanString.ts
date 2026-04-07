b816ab5349675c9023e8ef06572cf0320c859257
Yurig19 <yuri.zc36@gmail.com>
2026-04-07 09:23:40 -0300

    chore: remove parse utility helpers

--- src/utils/parseBooleanString.ts
+++
@@ -1,9 +0,0 @@
-import { BadRequestException } from '@nestjs/common';
-
-export function parseBooleanString(value: boolean | string, fieldName: string): boolean {
-  if (typeof value === 'boolean') return value;
-  if (value === 'true') return true;
-  if (value === 'false') return false;
-
-  throw new BadRequestException(`${fieldName} deve ser true ou false.`);
-}
