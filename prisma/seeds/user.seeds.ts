72f8134def0f22d4bfbadb4eea00221367ac48ed
Yurig19 <yuri.zc36@gmail.com>
2026-04-10 20:07:40 -0300

    feat(seeds): add seed entries for all user profile types

--- prisma/seeds/user.seeds.ts
+++ prisma/seeds/user.seeds.ts
@@ -1,14 +1,31 @@
-import { PrismaClient, Role, Status } from '@prisma/client';
+import { PrismaClient, Role, Status, UserProfileType } from '@prisma/client';
 import { hashSync } from 'bcrypt';
 
 export async function seedUser(prisma: PrismaClient) {
   await prisma.user.createMany({
     data: [
       {
-        name: 'user one',
-        email: 'user.one@email.com',
+        name: 'client one',
+        email: 'client.one@email.com',
         password: hashSync('12345678', 10),
         role: Role.User,
+        profileType: UserProfileType.Client,
+        status: Status.Active,
+      },
+      {
+        name: 'partner one',
+        email: 'partner.one@email.com',
+        password: hashSync('12345678', 10),
+        role: Role.User,
+        profileType: UserProfileType.Partner,
+        status: Status.Active,
+      },
+      {
+        name: 'delivery one',
+        email: 'delivery.one@email.com',
+        password: hashSync('12345678', 10),
+        role: Role.User,
+        profileType: UserProfileType.Delivery,
         status: Status.Active,
       },
     ],
